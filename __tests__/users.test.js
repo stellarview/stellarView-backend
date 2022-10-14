31525313778287103a3fe5dff25dad6b1d9175d5
austinbhan <austinbhan@outlook.com>
2022-10-14 11:31:19 -0700

    first auth testing

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -6,10 +6,11 @@ const UserService = require('../lib/services/UserService');
 
 // Dummy user for testing
 const mockUser = {
-  firstName: 'Test',
-  lastName: 'User',
+  username: 'User',
   email: 'test@example.com',
   password: '12345',
+  completed_categories: [''],
+  total_points: '0'
 };
 
 const registerAndLogin = async (userProps = {}) => {
@@ -36,15 +37,16 @@ describe('user routes', () => {
     pool.end();
   });
 
-  it('creates a new user', async () => {
+  it.only('creates a new user', async () => {
     const res = await request(app).post('/api/v1/users').send(mockUser);
-    const { firstName, lastName, email } = mockUser;
+    const { username, email, total_points } = mockUser;
 
     expect(res.body).toEqual({
       id: expect.any(String),
-      firstName,
-      lastName,
+      username,
       email,
+      completed_categories: null, 
+      total_points
     });
   });
 
@@ -80,8 +82,9 @@ describe('user routes', () => {
     await agent.post('/api/v1/users').send({
       email: 'admin',
       password: '1234',
-      firstName: 'admin',
-      lastName: 'admin',
+      username: 'admin',
+      completed_categories: [],
+      total_points: '0'
     });
     // sign in the user
     await agent
